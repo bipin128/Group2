a954c42d7ccb9462205dcf781bc98bc1763e21f7
Bipin <bkhatiwada140@gmail.com>
2020-08-21 12:27:18 -0500

    New Commit

--- src/main/java/com/vastika/util/DatabaseUtil.java
+++ src/main/java/com/vastika/util/DatabaseUtil.java
@@ -1,4 +1,20 @@
 package com.vastika.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseUtil {
+
+    public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String URl = "jdbc:mysql://localhost:3306/bipin";
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "bipin128";
+
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName(DRIVER_NAME);
+        return DriverManager.getConnection(URl, USER_NAME, PASSWORD);
+    }
 }
+
